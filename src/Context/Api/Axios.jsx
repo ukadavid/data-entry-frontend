bb09a6347e07b45a274a4ca40d10b37728607dda
Tobechukwu David Uka <ukadavid7@gmail.com>
2023-09-19 01:01:38 +0100

    env update

--- src/Context/Api/Axios.jsx
+++ src/Context/Api/Axios.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseUrl = import.meta.env.VITE_BASE_URL;
+// const baseUrl = import.meta.env.VITE_BASE_URL;
+const baseUrl = "https://dataentry-0j1n.onrender.com";
 
 // Get Request
 export const apiGet = (path) => {
